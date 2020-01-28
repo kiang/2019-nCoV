358f951f5a434a246a88c6f921b96c2d8bbe8e72
kiang <kiange@gmail.com>
2020-01-28 17:54:51 +0800

    fetch jhu data

---
+++ scripts/01_fetch_jhu.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'vendor/autoload.php';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+$filePath = dirname(__DIR__) . '/raw/jhu.edu';
+if(!file_exists($filePath)) {
+    mkdir($filePath, 0777, true);
+}
+$spreadsheetFile = $filePath . '/data.xlsx';
+file_put_contents($spreadsheetFile, file_get_contents('https://docs.google.com/spreadsheets/d/1yZv9w9zRKwrGTaR-YzmAqMefw4wMlaXocejdxZaTs6w/export?format=xlsx&id=1yZv9w9zRKwrGTaR-YzmAqMefw4wMlaXocejdxZaTs6w'));
+$spreadsheet = IOFactory::load($spreadsheetFile);
+$sheetIndex = 0;
+foreach ($spreadsheet->getAllSheets() AS $worksheet) {
+    $targetFile = $filePath . '/' . $worksheet->getTitle() . '.csv';
+    $targetFile = str_replace(' ', '_', $targetFile);
+    $spreadsheet->setActiveSheetIndex($sheetIndex);
+    $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
+    $writer->save($targetFile);
+    ++$sheetIndex;
+}
